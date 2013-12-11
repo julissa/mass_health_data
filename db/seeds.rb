79b12bbe81a67558a89bea9a2c7b6935e48b7e7d
faizaanshamsi <faizaanshamsi@gmail.com>
2013-12-11 14:49:54 -0500

    seeded database successfully BOOYAH!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+datafile = Rails.root + 'db/data/mass_health_data.csv'
+
+# TownHealthRecord.destroy_all
+
+CSV.foreach(datafile, headers: true) do |row|
+
+  row.each do |col|
+    unless col[1] == nil
+      col[1].gsub!('$', '')
+      col[1].gsub!(',', '')
+      col[1] = nil if col[1] == 'NA'
+    end
+
+  end
+
+  TownHealthRecord.find_or_create_by(
+    town: row[0],
+    total_population_2005: row[1],
+    population_0_to_19_2005: row[2],
+    population_greater_than_65_2005: row[3],
+    per_capita_income_2000: row[4],
+    persons_below_poverty_2000: row[5],
+    percent_persons_below_poverty_2000: row[6],
+    percent_adequacy_pre_natal_care: row[7],
+    percent_c_section_delivery_2005_to_2008: row[8],
+    total_infant_deaths_2005_to_2008: row[9],
+    infant_mortality_rate_per_thousand_2005_to_2008: row[10],
+    percent_low_birthweight_2005_to_2008: row[11],
+    percent_multiple_births_2005_to_2008: row[12],
+    percent_publicly_financed_prenatal_care_2005_to_2008: row[13],
+    percent_teen_births_2005_to_2008: row[14]
+    )
+end
